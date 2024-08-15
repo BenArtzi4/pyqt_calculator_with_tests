023ee3c39fc01f1fb25dbe5085cf6a084b236d2c
BenArtzi4 <benartzi4@gmail.com>
2024-08-15 17:05:59 +0300

    add unittest for substract and update format

--- test_logic_unittest.py
+++ test_logic_unittest.py
@@ -1,9 +1,16 @@
+# test_logic_unittest.py
 import unittest
+from logic import CalculatorActions
 
-
-class TestAddition(unittest.TestCase):
+class TestCalculatorActions(unittest.TestCase):
     def test_add_numbers(self):
-        self.assertEqual(add_numbers(1, 2), 3)
-        self.assertEqual(add_numbers(-1, 1), 0)
-        self.assertEqual(add_numbers(-1, -1), -2)
-        self.assertEqual(add_numbers(0, 0), 0)
+        self.assertEqual(CalculatorActions.add_numbers(1, 2), 3)
+        self.assertEqual(CalculatorActions.add_numbers(-1, 1), 0)
+        self.assertEqual(CalculatorActions.add_numbers(-1, -1), -2)
+        self.assertEqual(CalculatorActions.add_numbers(0, 0), 0)
+
+    def test_subtract_numbers(self):
+        self.assertEqual(CalculatorActions.subtract_numbers(10, 5), 5)
+        self.assertEqual(CalculatorActions.subtract_numbers(0, 0), 0)
+        self.assertEqual(CalculatorActions.subtract_numbers(-1, -1), 0)
+        self.assertEqual(CalculatorActions.subtract_numbers(-1, 1), -2)
