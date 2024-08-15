6f6a4e43ba83ac6cf1b55aeedcaae4377a60d368
BenArtzi4 <benartzi4@gmail.com>
2024-08-15 17:04:36 +0300

    add methods to logic file

---
+++ logic.py
@@ -0,0 +1,12 @@
+class CalculatorActions:
+    @staticmethod
+    def add_numbers(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract_numbers(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply_numbers(a, b):
+        return a * b
