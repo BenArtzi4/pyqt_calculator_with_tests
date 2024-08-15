880cc173a06bcf18b50a4a8c674de46d02ce43c3
BenArtzi4 <benartzi4@gmail.com>
2024-08-15 13:57:42 +0300

    write add pytests

---
+++ test_logic_pytest.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(-1, -1) == -2
+    assert add_numbers(0, 0) == 0
