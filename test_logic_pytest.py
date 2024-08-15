15cf0bdab73e4114f2ace3aa4a1d15966b878ac3
BenArtzi4 <benartzi4@gmail.com>
2024-08-15 17:06:50 +0300

    add substract to pytest

--- test_logic_pytest.py
+++ test_logic_pytest.py
@@ -1,7 +1,14 @@
-import pytest
+# test_logic_pytest.py
+from logic import CalculatorActions
 
 def test_add_numbers():
-    assert add_numbers(1, 2) == 3
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(-1, -1) == -2
-    assert add_numbers(0, 0) == 0
+    assert CalculatorActions.add_numbers(1, 2) == 3
+    assert CalculatorActions.add_numbers(-1, 1) == 0
+    assert CalculatorActions.add_numbers(-1, -1) == -2
+    assert CalculatorActions.add_numbers(0, 0) == 0
+
+def test_subtract_numbers():
+    assert CalculatorActions.subtract_numbers(10, 5) == 5
+    assert CalculatorActions.subtract_numbers(0, 0) == 0
+    assert CalculatorActions.subtract_numbers(-1, -1) == 0
+    assert CalculatorActions.subtract_numbers(-1, 1) == -2
