7c27f1207dd055db21e9f34778a70e302566ff01
BenArtzi4 <benartzi4@gmail.com>
2024-08-15 17:22:00 +0300

    update pytest tests format

--- test_logic_pytest.py
+++ test_logic_pytest.py
@@ -1,17 +1,20 @@
 # test_logic_pytest.py
 from logic import CalculatorActions
 
+
 def test_add_numbers():
     assert CalculatorActions.add_numbers(1, 2) == 3
     assert CalculatorActions.add_numbers(-1, 1) == 0
     assert CalculatorActions.add_numbers(-1, -1) == -2
     assert CalculatorActions.add_numbers(0, 0) == 0
 
+
 def test_subtract_numbers():
     assert CalculatorActions.subtract_numbers(10, 5) == 5
     assert CalculatorActions.subtract_numbers(0, 0) == 0
     assert CalculatorActions.subtract_numbers(-1, -1) == 0
     assert CalculatorActions.subtract_numbers(-1, 1) == -2
+
 
 def test_multiply_numbers():
     assert CalculatorActions.multiply_numbers(3, 4) == 12
