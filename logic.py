47a5209941b9b8bb53c2d96db30cdadf1303f851
BenArtzi4 <benartzi4@gmail.com>
2024-08-16 13:24:49 +0300

    fix actions input validate

--- logic.py
+++ logic.py
@@ -1,19 +1,24 @@
-# logic.py
 class CalculatorActions:
     @staticmethod
     def add_numbers(a, b):
+        if isinstance(a, bool) or isinstance(b, bool):
+            raise ValueError("Inputs must not be boolean.")
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-            raise ValueError("Both inputs must be numeric")
+            raise ValueError("Inputs must be numbers.")
         return a + b
 
     @staticmethod
     def subtract_numbers(a, b):
+        if isinstance(a, bool) or isinstance(b, bool):
+            raise ValueError("Inputs must not be boolean.")
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-            raise ValueError("Both inputs must be numeric")
+            raise ValueError("Inputs must be numbers.")
         return a - b
 
     @staticmethod
     def multiply_numbers(a, b):
+        if isinstance(a, bool) or isinstance(b, bool):
+            raise ValueError("Inputs must not be boolean.")
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-            raise ValueError("Both inputs must be numeric")
+            raise ValueError("Inputs must be numbers.")
         return a * b
